aa4a934a503c43b83600a3374525d510d9b05f77
root <d.m.m.pocorni@gmail.com>
2023-01-31 23:58:38 +0000

    first-commit

---
+++ my_app.py
@@ -0,0 +1,15 @@
+# from mylib import *
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+@app.route('/assignment')
+def assignment():
+    return 'This is my final assignment!'
+
---
+++ my_app_test.py
@@ -0,0 +1,14 @@
+import pytest 
+from my_app import app
+# from my  # Flask instance of the API
+
+def test_index_route():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Hello, world!'
+    
+def test_assignment_route():
+    response = app.test_client().get('/assignment')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'This is my final assignment!'
+    
